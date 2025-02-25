4946a5cb355a9f41cbee8ca173aaccfe328fe09a
MattiaMontini <143625012+MattiaMontini@users.noreply.github.com>
2025-02-25 12:58:55 +0100

    Update index.js

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3080;
+const PORT = 80;
 const persone = [];
 
 app.use(cors());
